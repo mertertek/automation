2d6eb40025a5f23a023373ffac8bbf91417043f1
Mert Ertek <mertertek1@gmail.com>
2024-07-25 12:51:47 +0300

    first

--- setup.py
+++ setup.py
@@ -5,12 +5,30 @@ DATA_FILES = ['automation.html']
 OPTIONS = {
     'argv_emulation': True,
     'packages': ['tkinter', 'bs4', 're'],
+    'plist': {
+        'CFBundleName': 'siu',
+        'CFBundleIdentifier': 'com.example.siu',
+        'CFBundleVersion': '1.0',
+        'CFBundleExecutable': 'siu',
+        'LSMinimumSystemVersion': '10.15',
+    }
 }
 
 setup(
-    name='Siu Html Updater',
+    name='siu',
     app=APP,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
+    install_requires=[
+        'beautifulsoup4==4.12.3',
+        'bs4==0.0.2',
+        'altgraph==0.17.4',
+        'macholib==1.16.3',
+        'modulegraph==0.19.6',
+        'packaging==24.1',
+        'py2app==0.28.8',
+        'setuptools==71.1.0',
+        'soupsieve==2.5',
+    ],
 )
